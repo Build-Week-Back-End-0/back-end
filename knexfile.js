01ee3bebbe0e4e13da3d4879b3e227eaddc00b9e
WillisLi <li.willis95@gmail.com>
2021-09-17 16:35:46 -0700

    heroku push

--- knexfile.js
+++ knexfile.js
@@ -1,48 +1,29 @@
-// require('dotenv').config()
+require('dotenv').config()
 
-// const pg = require('pg')
+const pg = require('pg')
 
-// if (process.env.DATABASE_URL) {
-//   pg.defaults.ssl = { rejectUnauthorized: false }
-// }
-
-// const sharedConfig = {
-//   client: 'pg',
-//   migrations: { directory: './api/data/migrations' },
-//   seeds: { directory: './api/data/seeds' },
-// }
-
-// module.exports = {
-//   development: {
-//     ...sharedConfig,
-//     connection: process.env.DEV_DATABASE_URL,
-//   },
-//   testing: {
-//     ...sharedConfig,
-//     connection: process.env.TESTING_DATABASE_URL,
-//   },
-//   production: {
-//     ...sharedConfig,
-//     connection: process.env.DATABASE_URL,
-//     pool: { min: 2, max: 10 },
-//   },
-// }
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
 
 const sharedConfig = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
+  client: 'pg',
   migrations: { directory: './api/data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  seeds: { directory: './api/data/seeds' },
 }
 
 module.exports = {
   development: {
     ...sharedConfig,
-    connection: { filename: './api/data/auth.db3' },
-    seeds: { directory: './api/data/seeds' },
+    connection: process.env.DEV_DATABASE_URL,
   },
   testing: {
     ...sharedConfig,
-    seeds: { directory: './api/data/seeds' },
+    connection: process.env.TESTING_DATABASE_URL,
   },
-};
+  production: {
+    ...sharedConfig,
+    connection: process.env.DATABASE_URL,
+    pool: { min: 2, max: 10 },
+  },
+}
