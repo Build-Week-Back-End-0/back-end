8fafb19865b5607486fdf528f6993e84525f20f0
JorshuaG <joshbgearheart@gmail.com>
2021-09-23 15:02:05 -1000

    added user id from token to GET users/plants

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,42 +1,48 @@
 const router = require("express").Router();
 const Users = require("./users-model.js");
-const { checkUserExists } = require('./users-middleware');
+const jwt = require("jsonwebtoken");
+const { checkUserExists } = require("./users-middleware");
 
 router.get("/", (req, res, next) => {
   Users.get()
-    .then(usersList => {
+    .then((usersList) => {
       res.status(200).json(usersList);
     })
     .catch(next);
 });
 
-router.get("/:id", (req, res, next) => {
-  const { id } = req.params;
-  Users.getById(id)
-    .then(currentUser => {
-      res.status(200).json(currentUser)
+router.get("/plants", checkUserExists, (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log(token);
+  const id = jwt.decode(token);
+  console.log(id);
+  // const { id } = req.params;
+  Users.getUserPlants(id.subject)
+    .then((selectedUser) => {
+      res.status(200).json(selectedUser);
     })
     .catch(next);
-}) 
+});
 
-router.get("/:id/plants", checkUserExists, (req, res, next) => {
+router.get("/:id", (req, res, next) => {
+  console.log("hello");
   const { id } = req.params;
-  Users.getUserPlants(id)
-    .then(selectedUser => {
-      res.status(200).json(selectedUser)
+  Users.getById(id)
+    .then((currentUser) => {
+      res.status(200).json(currentUser);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.put("/:id", checkUserExists, (req, res, next) => {
   const { id } = req.params;
   Users.update(id, req.body)
     .then(() => {
-      res.status(200).json({message: "User info updated"})
+      res.status(200).json({ message: "User info updated" });
     })
     .catch(() => {
-      res.status(500).json({message: "Invalid change."})
-    })
-})
- 
+      res.status(500).json({ message: "Invalid change." });
+    });
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 5000;
+
+server.use((req, res, next) => {
+  console.log("%0", req);
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
